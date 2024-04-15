9a575886918aeff6ca54441b810bbd9081bf5b95
shivani <rshivani58@gmail.com>
2024-04-15 19:31:08 -0400

    modified based on final assessment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,57 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext} from 'react';
 import { AppContext } from '../context/AppContext';
-
+const BUDGET_MAX_VALUE = 20000;
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+    const { budget, totalExpenses,currency, dispatch } = useContext(AppContext);
+  
+    const onChangeBudgetHandler = (event) => {
+      const enteredValue = Number(event.target.value);
+  
+      // check if the entered value is a number
+      if (Number.isNaN(enteredValue)) {
+        alert('Please enter a valid number.');
+        return;
+      }
+  
+      // check if the entered value is an integer number
+      if (!Number.isInteger(enteredValue)) {
+        alert('Please enter an integer number.');
+        return;
+      }
+  
+      if (enteredValue < totalExpenses) {
+        alert(
+          "You cannot reduce budget value lower than spending " +
+            currency +
+            totalExpenses
+        );
+      } else {
+        if (enteredValue > BUDGET_MAX_VALUE) {
+          alert('The Value cannot exceed ' + BUDGET_MAX_VALUE);
+          return;
+        }
+  
+        dispatch({
+          type: 'SET_BUDGET',
+          payload: enteredValue,
+        });
+      }
+    };
+  
     return (
+
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+<span>Budget: {currency}</span>
+          <input
+            required="required"
+            type="number"
+            id="budget"
+            value={budget}
+            step="10"
+            onChange={onChangeBudgetHandler}
+          ></input>
+        </div>
     );
-};
-export default Budget;
+  };
+  
+  export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
+import { FaPlusCircle,FaMinusCircle } from 'react-icons/fa';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch,currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -25,13 +26,27 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
+        <td className="col">{props.name}</td>
+        <td className="col">{currency}{props.cost}</td>
+        <td className="col"><FaPlusCircle size='1.2em' color="#4ead5c" onClick={event => increaseAllocation(props.name)}></FaPlusCircle></td>
+        <td className="col"><FaMinusCircle size='1.2em' color="#af1e11" onClick={event => decreaseAllocation(props.name)}></FaMinusCircle></td>
+        <td className="col"><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+    </tr>
     );
 };
 
